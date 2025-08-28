314de8ab1a0806ce56f48ed665e9c07705388629
filipondios <danialhamilla262@gmail.com>
2025-08-28 12:48:30 +0200

    delete c files

--- aes128.h
+++
@@ -1,8 +0,0 @@
-#ifndef _AES128_H
-#define _AES128_H
-#include <stdint.h>
-
-void aes128_encrypt(const uint8_t block[16], uint8_t cipher_block[16], const uint8_t key[16]);
-void aes128_decrypt(const uint8_t cipher_block[16], uint8_t block[16], const uint8_t key[16]);
-
-#endif // !_AES128_H
